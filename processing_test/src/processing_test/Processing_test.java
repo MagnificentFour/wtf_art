735d65ad399d69874d7f12c9c17fbdd7e7cc9023
noenik <niklas.noem@gmail.com>
2015-02-16 19:10:22 +0100

    Lagt til CannyEdgeDetector

--- processing_test/src/processing_test/Processing_test.java
+++ processing_test/src/processing_test/Processing_test.java
@@ -6,6 +6,13 @@
 
 package processing_test;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
 /**
  *
  * @author nikla_000
@@ -16,7 +23,15 @@ public class Processing_test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new DisplayFrame().setVisible(true);
+        //new DisplayFrame().setVisible(true);
+        BufferedImage in;
+        try {
+            in = ImageIO.read(new File("E:/image.jpg"));
+            CannyEdgeDetector ced = new CannyEdgeDetector(in, 40, 80);
+            ImageIO.write(ced.filter(), "jpg", new File("E:/newimage.jpg"));
+        } catch (IOException ex) {
+            System.out.println("No good");
+        }
     }
     
 }
