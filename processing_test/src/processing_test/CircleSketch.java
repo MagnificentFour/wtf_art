d2d703359c3e2189ab5dfd538fe3e673ef58f335
antring <emauring@hotmail.com>
2015-02-17 13:23:22 +0100

    Merge

--- processing_test/src/processing_test/CircleSketch.java
+++ processing_test/src/processing_test/CircleSketch.java
@@ -10,24 +10,40 @@ public class CircleSketch extends PApplet {
 
     float lastpnkt = 100;
     float lastpnkt2 = 100;
+    int sizeWidth = 400;
+    int sizeHeight = 400;
+    int drawStatus = 0;
+    int xK = 0;
+    int yK = 0;
 
     public void setup() {
-        size(400, 400);
+        size(sizeWidth, sizeHeight);
         background(255);
+        frameRate(120);
+        wtfSikring();
     }
 
     public void draw() {
-        //background(155);
-        float pnkt = random(400);
-        float pnkt2 = random(400);
-        fill(80, 150, 20);
-        if ((pnkt > lastpnkt - 25 && pnkt < lastpnkt + 25)
-                && (pnkt2 > lastpnkt2 - 25 || pnkt2 < lastpnkt2 + 25)) {
-            line(pnkt, pnkt2, pnkt + 20, pnkt2 + 20);
-            lastpnkt = pnkt;
-            lastpnkt2 = pnkt2;
+        for (int i = yK; i < yK + 11; i++) {
+            for (int x = xK; x < xK + 11; x++) {
+                stroke(random(255), random(255), random(255));
+                point(x, i);
+            }
         }
-        ellipseMode(CENTER);
-        ellipse(mouseX, mouseY, 40, 40);
+        xK += 10;
+
+        if (xK == sizeWidth) {
+            yK += 10;
+            xK = 0;
+        }
+    }
+    
+    public void wtfSikring() {
+        for (int x = 0; x < 11; x++) {
+            for (int y = 0; y < 11; y++) {
+                stroke(random(255), random(255), random(255));
+                point(x, y);
+            }
+        }
     }
 }
